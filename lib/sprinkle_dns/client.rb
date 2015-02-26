70a5c376a4d92e96c13ecd9d4029448a896ef0d3
Robert Pankowecki <robert.pankowecki@gmail.com>
2015-02-26 15:18:40 +0100

    Create missing zones

--- lib/sprinkle_dns/client.rb
+++ lib/sprinkle_dns/client.rb
@@ -2,6 +2,7 @@ require 'sprinkle_dns/zone_domain'
 require 'sprinkle_dns/zone_entry'
 require 'sprinkle_dns/core_ext'
 require 'sprinkle_dns/providers/aws_dns'
+require 'fog'
 
 module SprinkleDNS
   class Client
@@ -12,6 +13,12 @@ module SprinkleDNS
       @hosted_zones           = {}
       @aws_access_key_id      = aws_access_key_id
       @aws_secret_access_key  = aws_secret_access_key
+      @aws = SprinkleDNS::AwsDNS.new(aws_access_key_id, aws_secret_access_key)
+      @dns = Fog::DNS.new({
+        provider:              'AWS',
+        aws_access_key_id:     aws_access_key_id,
+        aws_secret_access_key: aws_secret_access_key,
+      })
     end
 
     def entry(type, name, value, ttl = 3600)
@@ -28,9 +35,16 @@ module SprinkleDNS
     end
 
     def sprinkle!
-      wanted_zones = self.hosted_zones
+      wanted_zones   = self.hosted_zones.keys
+      existing_zones = @aws.hosted_zones.keys
+      (wanted_zones - existing_zones).each do |new_zone|
+        @dns.create_hosted_zone(new_zone)
+      end
+      (existing_zones - wanted_zones).each do |destroyable_zone|
+        # TODO
+        puts "NOT deleteding #{destroyable_zone}"
+      end
 
-      # 1. Make sure all self.hosted_zones exists at AWS
       # 2. DIFF AWS.hosted_zones versus self.hosted_zones
       # 3. Show the diff nicely
       # 4. Push all changes to AWS
