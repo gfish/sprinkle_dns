28254d54fdf9d0dd5468a386cb170e40880424a1
Kasper Grubbe <kawsper@gmail.com>
2017-02-09 16:48:44 +0000

    Add more examples to specs

--- spec/unit/sprinkle_dns_spec.rb
+++ spec/unit/sprinkle_dns_spec.rb
@@ -5,12 +5,12 @@ RSpec.describe SprinkleDNS::Client do
     r53c = SprinkleDNS::Route53Client.new('1','2')
     sdns = SprinkleDNS::Client.new(r53c)
 
-    sdns.entry('A',  'kaspergrubbe.com',        '88.80.80.80', 60)
-    sdns.entry('A',  'assets.kaspergrubbe.com', '88.80.80.80', 60)
-    sdns.entry('MX', 'mail.kaspergrubbe.com',   ['10 mailserver.example.com', '20 mailserver2.example.com'], 300)
-    sdns.entry('MX', 'main.kaspergrubbe.com',   ['10 mailserver.example.com'], 300)
-    sdns.entry('A',  'streamy.kaspergrubbe.com.', '198.211.96.200', 60)
-    sdns.entry('A',  'blog.kaspergrubbe.com',     '198.211.96.200', 60)
+    sdns.entry('A',     'kaspergrubbe.com',             '88.80.80.80', 60)
+    sdns.entry('A',     'assets.kaspergrubbe.com',      '88.80.80.80', 60)
+    sdns.entry('MX',    'mail.kaspergrubbe.com',        ['10 mailserver.example.com', '20 mailserver2.example.com'], 300)
+    sdns.entry('MX',    'main.kaspergrubbe.com',        ['10 mailserver.example.com'], 300)
+    sdns.entry('A',     'streamy.kaspergrubbe.com.',    '198.211.96.200', 60)
+    sdns.entry('A',     'blog.kaspergrubbe.com',        '198.211.96.200', 60)
     sdns.entry('CNAME', 'www.es.kaspergrubbe.com',      "#{Time.now.to_i}.example.com.", 42, 'es.kaspergrubbe.com')
     sdns.entry('CNAME', 'staging.es.kaspergrubbe.com.', "#{Time.now.to_i}.example.com.", 42, 'es.kaspergrubbe.com.')
 
