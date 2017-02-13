381815164482cfbb75f03dec2812ae3dcc880dae
Kasper Grubbe <kawsper@gmail.com>
2017-02-13 14:04:31 +0000

    Rename HostedZone#changes_made? to modified?

--- lib/sprinkle_dns/hosted_zone.rb
+++ lib/sprinkle_dns/hosted_zone.rb
@@ -37,7 +37,7 @@ module SprinkleDNS
       @resource_record_sets.select{|hze| !hze.referenced?}
     end
 
-    def changes_made?
+    def modified?
       [entries_to_create, entries_to_update, entries_to_delete].map(&:size).inject(:+) > 0
     end
 
