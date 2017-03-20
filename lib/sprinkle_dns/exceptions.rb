f3e6a948c984d52b06c95fe7ec566b92beb6dd4e
Kasper Grubbe <kawsper@gmail.com>
2017-03-20 15:18:22 +0100

    Add failing specs

--- lib/sprinkle_dns/exceptions.rb
+++ lib/sprinkle_dns/exceptions.rb
@@ -2,4 +2,7 @@ module SprinkleDNS
   class RecordNotAString < StandardError; end
   class RecordNotValid   < StandardError; end
   class TtlNotInteger    < StandardError; end
+
+  class MissingHostedZones < StandardError; end
+  class DuplicatedHostedZones < StandardError; end
 end
