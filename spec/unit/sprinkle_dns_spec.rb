1671470b9b4fa6af8d0fc5235e56a419fedc0770
Kasper Grubbe <kawsper@gmail.com>
2019-04-16 15:58:00 +0100

    Add two pending cases with overwrites

--- spec/unit/sprinkle_dns_spec.rb
+++ spec/unit/sprinkle_dns_spec.rb
@@ -35,6 +35,32 @@ RSpec.describe SprinkleDNS::Client do
     expect(sdns.wanted_zones['billetto.com.'].count).to eq 2
   end
 
+  it 'should allow overwrites' do
+    r53c = SprinkleDNS::Route53Client.new('1','2')
+    sdns = SprinkleDNS::Client.new(r53c)
+
+    ['billetto.at', 'billetto.io', 'billetto.my'].each do |domain|
+      sdns.entry('A', domain, '88.80.80.80', 60)
+      sdns.entry('A', "www.#{domain}", '88.80.80.80', 60)
+    end
+    # Overwrite and null-route to localhost
+    sdns.entry('A', 'billetto.at', '127.0.0.1', 60)
+
+    pending
+    raise
+  end
+
+  it 'should allow overwrites of alias' do
+    r53c = SprinkleDNS::Route53Client.new('1','2')
+    sdns = SprinkleDNS::Client.new(r53c)
+
+    sdns.entry('A', "billetto.pl", '88.80.80.80', 60)
+    sdns.alias('A', 'billetto.pl', 'Z215JYRZR1TBD5', 'dualstack.mothership-prod-elb-546580691.eu-central-1.elb.amazonaws.com')
+
+    pending
+    raise
+  end
+
   context 'record validation' do
     it 'should only allow valid string records' do
       valid_records = ['SOA','A','TXT','NS','CNAME','MX','NAPTR','PTR','SRV','SPF','AAAA']
