3c54c922f5dfe973aa26488f3fa9339eb82fab96
Kasper Grubbe <kawsper@gmail.com>
2019-04-21 20:59:24 +0100

    Prepend hosted_zone_id and dns_name with "target_"

--- lib/sprinkle_dns/hosted_zone_alias.rb
+++ lib/sprinkle_dns/hosted_zone_alias.rb
@@ -1,36 +1,36 @@
 module SprinkleDNS
   class HostedZoneAlias
-    attr_accessor :type, :name, :hosted_zone_id, :dns_name, :hosted_zone
-    attr_accessor :changed_type, :changed_name, :changed_hosted_zone_id, :changed_dns_name
     attr_accessor :referenced
+    attr_accessor :type, :name, :hosted_zone, :target_hosted_zone_id, :target_dns_name
+    attr_accessor :changed_type, :changed_name, :changed_target_hosted_zone_id, :changed_target_dns_name
 
-    def initialize(type, name, hosted_zone_id, dns_name, hosted_zone)
+    def initialize(type, name, target_hosted_zone_id, target_dns_name, hosted_zone)
       @type           = type
       @name           = zonify!(name)
       @hosted_zone    = hosted_zone
 
-      @hosted_zone_id         = hosted_zone_id
-      @changed_hosted_zone_id = false
-      @original_hosted_zone_id       = hosted_zone_id.clone
+      @target_hosted_zone_id          = target_hosted_zone_id
+      @changed_target_hosted_zone_id  = false
+      @original_target_hosted_zone_id = target_hosted_zone_id.clone
 
-      @dns_name          = dns_name
-      @changed_dns_name  = false
-      @original_dns_name = dns_name.clone
+      @target_dns_name          = target_dns_name
+      @changed_target_dns_name  = false
+      @original_target_dns_name = target_dns_name.clone
 
-      raise if [@type, @name, @hosted_zone_id, @dns_name, @hosted_zone].any?(&:nil?)
-      @referenced    = false
+      raise if [@type, @name, @target_hosted_zone_id, @target_dns_name, @hosted_zone].any?(&:nil?)
+      @referenced = false
     end
 
     def mark_new!
-      @changed_type, @changed_name, @changed_hosted_zone_id, @changed_dns_name = [true, true, true, true]
+      @changed_type, @changed_name, @changed_target_hosted_zone_id, @changed_target_dns_name = [true, true, true, true]
     end
 
     def new?
-      [@changed_type, @changed_name, @changed_hosted_zone_id, @changed_dns_name].all?
+      [@changed_type, @changed_name, @changed_target_hosted_zone_id, @changed_target_dns_name].all?
     end
 
     def changed?
-      [@changed_type, @changed_name, @changed_hosted_zone_id, @changed_dns_name].any?
+      [@changed_type, @changed_name, @changed_target_hosted_zone_id, @changed_target_dns_name].any?
     end
 
     def mark_referenced!
@@ -57,8 +57,8 @@ module SprinkleDNS
         "Alias",
         sprintf("%4s", type),
         sprintf("%30s", name),
-        sprintf("%10s", hosted_zone_id),
-        sprintf("%30s", dns_name),
+        sprintf("%10s", target_hosted_zone_id),
+        sprintf("%30s", target_dns_name),
         sprintf("%6s", hosted_zone),
       ].join(" ")
     end
